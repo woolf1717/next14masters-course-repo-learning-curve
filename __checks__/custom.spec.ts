635e52997e1ef1122520c1c8466aecc5620dcead
woolf1717 <Barstpiczynski@gmail.com>
2024-02-10 10:49:18 +0100

    Checkly fix - proper url

--- __checks__/custom.spec.ts
+++ __checks__/custom.spec.ts
@@ -4,7 +4,7 @@ import { expect, test } from "@playwright/test";
 // test.setTimeout(60_000);
 
 test("Custom Browser Check", async ({ page }) => {
-  const response = await page.goto("http://localhost:3000/");
+  const response = await page.goto("https://next14masters-shop.vercel.app/");
   expect(response?.status()).toBeLessThan(400);
   await page.screenshot({ path: "screenshot.jpg" });
 });
